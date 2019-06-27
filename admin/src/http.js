9dddd4175546e3efeb27f0791d514b9f4979f43b
mytheart <550703617@qq.com>
2019-06-27 18:11:44 +0800

    初步完成上线

--- admin/src/http.js
+++ admin/src/http.js
@@ -3,7 +3,7 @@ import Vue from 'vue'
 import router from './router'
 
 const http = axios.create({
-  baseURL: 'http://localhost:3000/admin/api'
+  baseURL: 'http://localhost:80/admin/api'
 })
 http.interceptors.request.use(function (config) {
   // Do something before request is sent
