05dfa6788ef895a713230ee20ef76561886c5ca3
mytheart <550703617@qq.com>
2019-06-27 18:17:05 +0800

    test

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -51,7 +51,7 @@ module.exports = app => {
   const upload = multer({ dest: __dirname + '/../../uploads' })
   app.post('/admin/api/upload', authMiddleware(), upload.single('file'), async (req, res) => {
     const file = req.file
-    file.url = `http://localhost:80/uploads/${file.filename}`
+    file.url = `http://120.79.182.51/uploads/${file.filename}`
     res.send(file)
   })
 
